ecaeaf1c9fe09c6b24470dc7f28b28012f57d521
Gabriel-Dupla <gabrielduplacontato@gmail.com>
2023-05-30 14:40:43 -0300

    aula4

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-    .then(resposta => resposta.json())
-    .then(r => {
-        if (r.erro) {
-            throw Error('Esse cep não existe!')
+async function buscaEndereco(cep) {
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP não existente')
 
-        } else 
-        console.log(r)
-    })
-    .catch(erro => console.log(erro))
-    .finally(mensagem => console.log('Processamento concluído'));
+        }
+        console.log(consultaCEPConvertida)
+        return consultaCEPConvertida
+    } catch (erro) {
+        console.log(erro);
+    }
+}
 
-
-
-
-console.log(consultaCEP)
